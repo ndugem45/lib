e30a392a18f07fb02c4d3bfc564265635cffbc1c
Tom <ndugem45@gmail.com>
2025-08-20 11:20:47 +0700

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   let acc = document.querySelectorAll("[data-accordion='head']");
   let body = document.querySelectorAll("[data-accordion='body']");
-  let i;
   
   body.forEach((elem, i)=>{
     elem.style.maxHeight = null;
   })
   
   acc.forEach((elem, i)=>{
-    elem[i].addEventListener("click", function() {
+    elem.addEventListener("click", function() {
       this.classList.toggle("active");
       let panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
