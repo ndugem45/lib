f295cfde9bb9417c47861a6972107886f565c2b3
Tom <ndugem45@gmail.com>
2025-10-09 14:16:21 +0700

    Update auto-collapse-navbar.js

--- auto-collapse-navbar.js
+++ auto-collapse-navbar.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let lastScrollTop = 0;
     let navbar = document.querySelector("[data-el='navbar']");
+    let btnNav = document.querySelector(".w-nav-button");
     let threshold = navbar.getAttribute('data-el-thresshold') ?? 100;
 
     navbar.style.transition = 'all 200ms ease';
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.attributeName === 'aria-expanded') {
+                btnNav.classList.add('show');
+                setTimeout(() => navbar.classList.remove('show'), 300);
+            }
+        });
+    });
+
+  observer.observe(btnNav, { attributes: true, attributeFilter: ['aria-expanded'] });
     
     window.addEventListener('scroll', function () {
         const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
