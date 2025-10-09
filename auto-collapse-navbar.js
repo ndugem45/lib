0de7e97e2491bcc3a2f646e08de7fe1125e23d7f
Tom <ndugem45@gmail.com>
2025-10-09 12:09:56 +0700

    Create auto-collapse-navbar.js

---
+++ auto-collapse-navbar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let lastScrollTop = 0;
+    let navbar = document.querySelector("[data-el='navbar']");
+    let threshold = navbar.getAttribute('data-el-thresshold') ?? 100;
+    
+    window.addEventListener('scroll', function () {
+      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+      if (Math.abs(currentScroll - lastScrollTop) > threshold) {
+        if (currentScroll > lastScrollTop) {
+            if(document.querySelector('.w-nav-button').getAttribute('aria-expanded').toLowerCase() == 'false'){
+                navbar.classList.add('hidden')    
+            }
+        } else {
+            navbar.classList.remove('hidden')
+        }
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+      }
+    });
+}, false);
