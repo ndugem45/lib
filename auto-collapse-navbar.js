cc1f816a1e278b7ff09eabef4fa157a02db6483f
Tom <ndugem45@gmail.com>
2025-10-09 13:14:15 +0700

    Update auto-collapse-navbar.js

--- auto-collapse-navbar.js
+++ auto-collapse-navbar.js
@@ -2,23 +2,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let lastScrollTop = 0;
     let navbar = document.querySelector("[data-el='navbar']");
     let threshold = navbar.getAttribute('data-el-thresshold') ?? 100;
+
+    navbar.style.transition = 'all 200ms ease';
     
     window.addEventListener('scroll', function () {
         const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
         if(currentScroll > threshold){
-            navbar.classList.add('show')
+            navbar.classList.add('show');
         }else{
-            navbar.classList.remove('show')
+            navbar.classList.remove('show');
         }
     
         if (Math.abs(currentScroll - lastScrollTop) > threshold) {
             if (currentScroll > lastScrollTop) {
                 if(document.querySelector('.w-nav-button').getAttribute('aria-expanded').toLowerCase() == 'false'){
-                    navbar.classList.add('hidden')    
+                    navbar.classList.add('hidden') ;   
                 }
             } else {
-                navbar.classList.remove('hidden')
+                navbar.classList.remove('hidden');
             }
             lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
         }
